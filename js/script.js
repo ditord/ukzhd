d24b97d7ad69d390aa9b520d5b79597e21ec6c0f
Artur Papyan <angry.root@gmail.com>
2020-07-11 22:53:57 +0400

    updated times

--- js/script.js
+++ js/script.js
@@ -1,35 +1,34 @@
 // Get the clients location from the form
 
-document.getElementById("submit").onclick = function(){
+document.getElementById("submit").onclick = function() {
 
-  var loc = document.getElementById("city").value;
-  console.log(loc)
+    var loc = document.getElementById("city").value;
+    console.log(loc)
 
-// Get the current hour and minute
-  var now = new Date();
-  var hh = now.getHours();
-  var mm = now.getMinutes();
-  console.log(now)
-  console.log(hh)
-  console.log(mm)
+    // Get the current hour and minute
+    var now = new Date();
+    var hh = now.getHours();
+    var mm = now.getMinutes();
+    console.log(now)
+    console.log(hh)
+    console.log(mm)
 
-// Check for conditions based on location and time and printout the departure time
-  if (loc == 1 && hh > 19){
-        document.getElementById("nexttrain").value = "8:25";
-  }
-  else if(loc == 1 && hh >= 8 && mm >= 25){
-          document.getElementById("nexttrain").value = "11:45";
-  }
-  else if (loc == 1 && hh <= 18 && hh <= 5){
-          document.getElementById("nexttrain").value = "18:05";
-  }
-  else if (loc == 2 && hh > 19){
+    // Check for conditions based on location and time and printout the departure time
+    if (loc == 1 && hh > 19) {
+        document.getElementById("nexttrain").value = "7:45";
+    } else if (loc == 1 && hh >= 8) {
+        document.getElementById("nexttrain").value = "11:55";
+    } else if (loc == 1 && hh <= 16) {
+        document.getElementById("nexttrain").value = "17:55";
+    } else if (loc == 1 && hh <= 18) {
+        document.getElementById("nexttrain").value = "18:15";
+    } else if (loc == 2 && hh > 19) {
         document.getElementById("nexttrain").value = "7:55";
-  }
-  else if (loc == 2 && hh > 7 && mm > 55){
-            document.getElementById("nexttrain").value = "14:30";
-  }
-  else if (loc == 2 && hh <= 18 && hh < 15){
-            document.getElementById("nexttrain").value = "18:15";
-  }
+    } else if (loc == 2 && hh >= 8) {
+        document.getElementById("nexttrain").value = "9:15";
+    } else if (loc == 2 && hh <= 13) {
+        document.getElementById("nexttrain").value = "14:15";
+    } else if (loc == 2 && hh <= 17) {
+        document.getElementById("nexttrain").value = "18:25";
+    }
 }
