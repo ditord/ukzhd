a29a6b234db2d6e88d7527162d8dc3bfd8b5fc46
Robert Papyan <robert.papyan@gmail.com>
2020-07-14 17:21:40 +0400

    laksdfj

--- js/script.js
+++ js/script.js
@@ -14,21 +14,34 @@ document.getElementById("submit").onclick = function() {
     console.log(mm)
 
     // Check for conditions based on location and time and printout the departure time
-    if (loc == 1 && hh > 19) {
+    if (loc == 1 && hh>19) {
         document.getElementById("nexttrain").value = "7:45";
-    } else if (loc == 1 && hh >= 8) {
-        document.getElementById("nexttrain").value = "11:55";
-    } else if (loc == 1 && hh <= 16) {
-        document.getElementById("nexttrain").value = "17:55";
-    } else if (loc == 1 && hh <= 18) {
+     }
+     else if (loc==1 && hh>=18) {
         document.getElementById("nexttrain").value = "18:15";
-    } else if (loc == 2 && hh > 19) {
+    }
+    else  if(loc==1 && hh>=12){
+        document.getElementById("nexttrain").value = "17:55";
+    }
+    else if (loc==1 && hh>=8) {
+        document.getElementById("nexttrain").value = "11:55";
+    }
+    else if(loc ==1){
+        document.getElementById("nexttrain").value = "7:45";
+    }
+    else if(loc==2 && hh>=19){
         document.getElementById("nexttrain").value = "7:55";
-    } else if (loc == 2 && hh >= 8) {
-        document.getElementById("nexttrain").value = "9:15";
-    } else if (loc == 2 && hh <= 13) {
-        document.getElementById("nexttrain").value = "14:15";
-    } else if (loc == 2 && hh <= 17) {
+    }
+    else if(loc == 2 && hh>=15 ){
         document.getElementById("nexttrain").value = "18:25";
+    }
+    else if(loc==2 && hh>=10){
+        document.getElementById("nexttrain").value = "14:15";
+    }
+    else if(loc==2 && hh>=8){
+        document.getElementById("nexttrain").value = "9:15";
+    }
+    else if(loc==2){
+        document.getElementById("nexttrain").value = "7:55";
     }
 }
