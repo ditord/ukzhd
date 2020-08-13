0b1d7761988d049bb1b9d32f75fd68b254656375
Artur Papyan <angry.root@gmail.com>
2020-08-13 13:27:48 +0400

    added minutes

--- js/script.js
+++ js/script.js
@@ -14,31 +14,31 @@ document.getElementById("submit").onclick = function () {
     console.log(mm)
 
     // Check for conditions based on location and time and printout the departure time
-    if (loc == 1 && hh > 19) {
+    if (loc == 1 && hh >= 18 && mm > 15) {
         document.getElementById("nexttrain").value = "7:45";
     }
-    else if (loc == 1 && hh >= 18) {
+    else if (loc == 1 && hh >= 17 && mm > 55) {
         document.getElementById("nexttrain").value = "18:15";
     }
-    else if (loc == 1 && hh >= 12) {
+    else if (loc == 1 && hh >= 11 && mm > 55) {
         document.getElementById("nexttrain").value = "17:55";
     }
-    else if (loc == 1 && hh >= 8) {
+    else if (loc == 1 && hh >= 7 && mm > 45) {
         document.getElementById("nexttrain").value = "11:55";
     }
     else if (loc == 1) {
         document.getElementById("nexttrain").value = "7:45";
     }
-    else if (loc == 2 && hh >= 19) {
+    else if (loc == 2 && hh >= 18 && mm > 25) {
         document.getElementById("nexttrain").value = "7:55";
     }
-    else if (loc == 2 && hh >= 15) {
+    else if (loc == 2 && hh >= 14 && mm > 25) {
         document.getElementById("nexttrain").value = "18:25";
     }
-    else if (loc == 2 && hh >= 10) {
+    else if (loc == 2 && hh >= 9 && mm > 15) {
         document.getElementById("nexttrain").value = "14:15";
     }
-    else if (loc == 2 && hh >= 8) {
+    else if (loc == 2 && hh >= 7 && mm > 55) {
         document.getElementById("nexttrain").value = "9:15";
     }
     else if (loc == 2) {
