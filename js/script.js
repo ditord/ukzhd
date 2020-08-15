d7f536387cf312b0721e887a03b85e85589d579e
Robert Papyan <robert.papyan@gmail.com>
2020-08-15 20:35:15 +0400

    Removed button, adjusted date, time selecors, changed css a little

--- js/script.js
+++ js/script.js
@@ -1,47 +1,88 @@
 // Get the clients location from the form
 
-document.getElementById("submit").onclick = function () {
 
-    var loc = document.getElementById("city").value;
-    console.log(loc)
+function showNextTrain() {
+
+    let loc = document.getElementById("city").value;
+
 
     // Get the current hour and minute
-    var now = new Date();
-    var hh = now.getHours();
-    var mm = now.getMinutes();
+    let now = new Date();
+    let hh = now.getHours();
+    let mm = now.getMinutes();
+
+    // Check for conditions based on location and time and printout the departure time
+    //For Gyumri
+    if (loc == 1) {
+        if (hh >= 19) {
+            document.getElementById("nexttrain").value = "7:45";
+        } else if (hh >= 17) {
+            document.getElementById("nexttrain").value = "18:15";
+        } else if (hh >= 12) {
+            document.getElementById("nexttrain").value = "17:55";
+        } else if (hh >= 8) {
+            document.getElementById("nexttrain").value = "11:55";
+        } else {
+            document.getElementById("nexttrain").value = "7:45";
+        }
+
+
+
+        if (hh == 18 && mm >= 15) {
+            document.getElementById("nexttrain").value = "7:45";
+        }
+        if (hh == 17 && mm >= 55) {
+            document.getElementById("nexttrain").value = "18:15";
+        }
+        if (hh == 11 && mm >= 55) {
+            document.getElementById("nexttrain").value = "17:55";
+        }
+        if (hh == 7 && mm >= 45) {
+            document.getElementById("nexttrain").value = "11:55";
+        }
+        if (hh == 17 && mm < 55) {
+            document.getElementById("nexttrain").value = "17:55";
+        }
+
+
+
+
+    }
+    //For Yerevan
+    if (loc == 2) {
+        if (hh >= 19) {
+            document.getElementById("nexttrain").value = "7:55";
+        } else if (hh >= 15) {
+            document.getElementById("nexttrain").value = "18:25";
+        } else if (hh >= 10) {
+            document.getElementById("nexttrain").value = "14:15";
+        } else if (hh >= 8) {
+            document.getElementById("nexttrain").value = "9:15";
+        } else {
+            document.getElementById("nexttrain").value = "7:55";
+        }
+
+        if (hh == 18 && mm >= 25) {
+            document.getElementById("nexttrain").value = "7:55";
+        }
+        if (hh == 14 && mm >= 25) {
+            document.getElementById("nexttrain").value = "18:25";
+        }
+        if (hh == 9 && mm >= 15) {
+            document.getElementById("nexttrain").value = "14:15";
+        }
+        if (hh == 7 && mm >= 55) {
+            document.getElementById("nexttrain").value = "9:15";
+        }
+
+
+    }
+
+    //console.log(loc)
+    console.log(loc)
     console.log(now)
     console.log(hh)
     console.log(mm)
-
-    // Check for conditions based on location and time and printout the departure time
-    if (loc == 1 && hh >= 18 && mm > 15) {
-        document.getElementById("nexttrain").value = "7:45";
-    }
-    else if (loc == 1 && hh >= 17 && mm > 55) {
-        document.getElementById("nexttrain").value = "18:15";
-    }
-    else if (loc == 1 && hh >= 11 && mm > 55) {
-        document.getElementById("nexttrain").value = "17:55";
-    }
-    else if (loc == 1 && hh >= 7 && mm > 45) {
-        document.getElementById("nexttrain").value = "11:55";
-    }
-    else if (loc == 1) {
-        document.getElementById("nexttrain").value = "7:45";
-    }
-    else if (loc == 2 && hh >= 18 && mm > 25) {
-        document.getElementById("nexttrain").value = "7:55";
-    }
-    else if (loc == 2 && hh >= 14 && mm > 25) {
-        document.getElementById("nexttrain").value = "18:25";
-    }
-    else if (loc == 2 && hh >= 9 && mm > 15) {
-        document.getElementById("nexttrain").value = "14:15";
-    }
-    else if (loc == 2 && hh >= 7 && mm > 55) {
-        document.getElementById("nexttrain").value = "9:15";
-    }
-    else if (loc == 2) {
-        document.getElementById("nexttrain").value = "7:55";
-    }
 }
+showNextTrain();
+setInterval("showNextTrain()", 1000);
